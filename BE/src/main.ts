e76180ae97eebea98ebb86756acdb450cdd6b5a7
Quatn <nguyenminhquanvh@gmail.com>
2025-11-06 10:52:35 +0700

    be/base: + Added the following packages: @nestjs/swagger swagger-ui-express class-validator class-transformer @nestjs/passport passport @nestjs/jwt passport-jwt @types/passport-jwt + Configured swagger

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  // Swagger setup
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('ERP Backend API')
+    .setDescription('API documentation for the ERP system')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'access-token',
+    ) // enables "Authorize" button for Swagger
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
