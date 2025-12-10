621cb7d3b5a0fadd304f83fae6c2f38aa329a69d
Quatn <nguyenminhquanvh@gmail.com>
2025-12-10 19:37:49 +0700

    feat(dashboard): Mock up for manufacturing order Quarterly Order Statuses Chart and Monthly Production Chart

--- FE-DESKTOP/src/app/(authenticated)/(main)/dashboard/page.tsx
+++ FE-DESKTOP/src/app/(authenticated)/(main)/dashboard/page.tsx
@@ -1,4 +1,8 @@
-import { Box, GridItem, SimpleGrid, Text } from "@chakra-ui/react";
+import { ManufacturingOrderMonthlyProductionBarChart } from "@/components/manufacturing-order/dashboard/monthly-production-chart/BarChart";
+import { ManufacturingOrderMonthlyProductionBarChartMonthSelector } from "@/components/manufacturing-order/dashboard/monthly-production-chart/MonthSelector";
+import { ManufacturingOrderStatusesPieChart } from "@/components/manufacturing-order/dashboard/quarterly-order-statuses-chart/PieChart";
+import { ManufacturingOrderMonthlyProductionChartProvider } from "@/context/manufacturing-order/dashboard/manufacturingOrderMonthlyProductionChartContext";
+import { Box, GridItem, Heading, HStack, SimpleGrid, Text } from "@chakra-ui/react";
 
 export default function Dashboard() {
   return (
@@ -14,18 +18,33 @@ export default function Dashboard() {
       <Text fontWeight={"semibold"} color={"fg"}>
         Dashboard
       </Text>
-        <SimpleGrid columns={{md: 1, lg: 2}} gap="40px" mx={5}>
-          <GridItem colSpan={{ base: 1 }}>
-          </GridItem>
-          <GridItem colSpan={{ base: 1 }}>
-            <Box bg="bg" p={2} pt={5} rounded={"sm"}>
+      <SimpleGrid columns={{ md: 1, lg: 2 }} gap="40px" mx={5}>
+        <GridItem colSpan={{ base: 1 }}>
+        </GridItem>
+
+        <GridItem colSpan={{ base: 1 }}>
+          <Box bg="bg" p={2} rounded={"sm"}>
+            <Heading size={"sm"}>Tổng quan trạng thái của các lệnh sản xuất theo quý</Heading>
+            <ManufacturingOrderStatusesPieChart />
+          </Box>
+        </GridItem>
+
+        <GridItem colSpan={{ base: 1, lg: 2 }}>
+          <ManufacturingOrderMonthlyProductionChartProvider>
+            <Box bg="bg" p={2} rounded={"sm"}>
+              <HStack mb={5} justifyContent={"space-between"}>
+                <Heading size={"sm"}>Số lượng lệnh sản xuất theo tháng</Heading>
+                <ManufacturingOrderMonthlyProductionBarChartMonthSelector />
+              </HStack>
+
+              <ManufacturingOrderMonthlyProductionBarChart />
             </Box>
-          </GridItem>
-          <GridItem colSpan={{ base: 1 }}>
-          </GridItem>
-          <GridItem colSpan={{ base: 1 }}>
-          </GridItem>
-        </SimpleGrid>
+          </ManufacturingOrderMonthlyProductionChartProvider>
+        </GridItem>
+
+        <GridItem colSpan={{ base: 1 }}>
+        </GridItem>
+      </SimpleGrid>
     </Box>
   );
 }
